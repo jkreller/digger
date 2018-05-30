8671d46379b019736386b73a4a6098e5749521b6
Byron Zeitschel <bzeitschel@eos-uptrade.de>
2018-05-30 16:59:06 +0200

    moving left and right

--- Assets/Scripts/MoleAnimation.cs
+++ Assets/Scripts/MoleAnimation.cs
@@ -6,27 +6,36 @@ public class MoleAnimation : MonoBehaviour {
 
     Animator animator;
     public float speed;
+    SpriteRenderer spriteRenderer;
 
 	void Start () {
 
         animator = GetComponent<Animator>();
-	}
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
 
         float move = Input.GetAxis("Horizontal");
 
         if(move > 0){
 
+            spriteRenderer.flipX = false;
             transform.Translate(Vector2.right * Time.deltaTime*speed);
             animator.SetBool("move", true);
+
+
         }
         else if(move < 0){
 
             transform.Translate(Vector2.left * Time.deltaTime*speed);
             animator.SetBool("move", true);
-
+            spriteRenderer.flipX = true;
+           
         }
         else{
 
