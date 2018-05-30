c1db4f76a92ec359e077c42cfcd04a267e07819f
Julian Kreller <jkreller@eos-uptrade.de>
2018-05-30 14:51:27 +0200

    Strichmännchen gesliced

--- Assets/Scripts/PixelPerfectCamera.cs
+++ Assets/Scripts/PixelPerfectCamera.cs
@@ -1,20 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class PixelPerfectCamera : MonoBehaviour {
-    public static float pixelsToUnit = 1f;
+public class PixelPerfectCamera : MonoBehaviour
+{
+
+    public static float pixelsToUnits = 1f;
     public static float scale = 1f;
 
     public Vector2 nativeResolution = new Vector2(240, 160);
 
-    void Awake() {
+    void Awake()
+    {
         var camera = GetComponent<Camera>();
 
-        if (camera.orthographic) {
+        if (camera.orthographic)
+        {
             scale = Screen.height / nativeResolution.y;
-            pixelsToUnit *= scale;
-            camera.orthographicSize = (Screen.height/ 2f) / pixelsToUnit;
+            pixelsToUnits *= scale;
+            camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
         }
     }
+
 }
