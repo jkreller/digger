7be8415f78f6bbdbe0fec4a2bb39d8938c9f947f
Byron Zeitschel <bzeitschel@eos-uptrade.de>
2018-05-30 16:33:19 +0200

    bewegung

---
+++ Assets/Scripts/MoleAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleAnimation : MonoBehaviour {
+
+    Animator animator;
+    public float speed;
+
+	void Start () {
+
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float move = Input.GetAxis("Horizontal");
+
+        if(move > 0){
+
+            transform.Translate(Vector2.right * Time.deltaTime*speed);
+            animator.SetBool("move", true);
+        }
+        else if(move < 0){
+
+            transform.Translate(Vector2.left * Time.deltaTime*speed);
+            animator.SetBool("move", true);
+
+        }
+        else{
+
+            animator.SetBool("move", false);
+
+        }
+
+		
+	}
+
+
+}
