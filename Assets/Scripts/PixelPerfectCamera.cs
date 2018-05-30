8ef151202882cd478b27cafec98cade8005c5c6c
Julian Kreller <jkreller@eos-uptrade.de>
2018-05-30 17:01:13 +0200

    Spieler

--- Assets/Scripts/PixelPerfectCamera.cs
+++ Assets/Scripts/PixelPerfectCamera.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class PixelPerfectCamera : MonoBehaviour
 {
-
     public static float pixelsToUnits = 1f;
     public static float scale = 1f;
 
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 150f;
+    public Vector2 maxVelocity = new Vector2(60, 100);
+
+    private Rigidbody2D body2D;
+    private SpriteRenderer renderer2D;
+
+	// Use this for initialization
+	void Start () {
+        body2D = GetComponent<Rigidbody2D>();
+        renderer2D = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var absValX = Mathf.Abs(body2D.elocity.x);
+	}
+}
