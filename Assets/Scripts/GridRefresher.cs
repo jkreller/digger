a85537a41b35f4cda3e4acb90c6cea63d96abb95
Julian Kreller <jkreller@eos-uptrade.de>
2018-09-27 15:13:24 +0200

    - Fix bugging tilemaps

---
+++ Assets/Scripts/GridRefresher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridRefresher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var tilemaps = FindObjectsOfType<Tilemap>();
+
+        foreach (Tilemap tilemap in tilemaps) {
+            tilemap.RefreshAllTiles();
+        }
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    
+    //}
+}
