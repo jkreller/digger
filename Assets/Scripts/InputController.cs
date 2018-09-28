772ad1642efff24264dd5420ff6f2096a6e72c78
bzeitschel <bzeitschel@eos-uptrade.de>
2018-09-28 07:46:22 +0200

    pause

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -13,23 +13,26 @@ public class InputController : MonoBehaviour
     public bool isLookingDown;
     [HideInInspector]
     public bool isLookingUp;
-
+    [HideInInspector]
+    public bool pause;
     private Joystick joystick;
     private GameObject mobileControlsObject;
     private bool onMobileDevice;
+
 
     /*
      * Use this for initialization
      */
     private void Start()
     {
-        #if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
+#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
             onMobileDevice = true;
-        #endif
+#endif
 
         joystick = FindObjectOfType<Joystick>();
 
         mobileControlsObject = GameObject.Find("MobileControls");
+
     }
 
     /*
@@ -37,45 +40,49 @@ public class InputController : MonoBehaviour
      */
     void Update()
     {
-        if (!onMobileDevice)
+        if (!pause)
         {
-            // controls for not mobile devices
-
-            // disable mobile controls
-            if (joystick && mobileControlsObject)
+            if (!onMobileDevice)
             {
-                joystick.enabled = false;
-                mobileControlsObject.SetActive(false);
-            }
+                // controls for not mobile devices
 
-            // moving
-            movingHorizontal = Input.GetAxisRaw("Horizontal");
+                // disable mobile controls
+                if (joystick && mobileControlsObject)
+                {
+                    joystick.enabled = false;
+                    mobileControlsObject.SetActive(false);
+                }
 
-            // jumping
-            isJumping = Input.GetButtonDown("Jump");
-
-            // digging
-            isDigging = Input.GetKey("left shift");
-
-            // looking up
-            isLookingUp = Input.GetKey("up");
-
-            // looking up
-            isLookingDown = Input.GetKey("down");
-        }
-        else
-        {
-            // controls for mobile devices
-
-            if (joystick) {
                 // moving
-                movingHorizontal = (joystick.Vertical < 0.3 && joystick.Vertical > -0.3) ? joystick.Horizontal : 0;
+                movingHorizontal = Input.GetAxisRaw("Horizontal");
+
+                // jumping
+                isJumping = Input.GetButtonDown("Jump");
+
+                // digging
+                isDigging = Input.GetKey("left shift");
 
                 // looking up
-                isLookingUp = joystick.Vertical > 0;
+                isLookingUp = Input.GetKey("up");
 
                 // looking up
-                isLookingDown = joystick.Vertical < 0;
+                isLookingDown = Input.GetKey("down");
+            }
+            else
+            {
+                // controls for mobile devices
+
+                if (joystick)
+                {
+                    // moving
+                    movingHorizontal = (joystick.Vertical < 0.3 && joystick.Vertical > -0.3) ? joystick.Horizontal : 0;
+
+                    // looking up
+                    isLookingUp = joystick.Vertical > 0;
+
+                    // looking up
+                    isLookingDown = joystick.Vertical < 0;
+                }
             }
         }
     }
