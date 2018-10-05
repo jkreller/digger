9f80a0586b356a2ba97ea114dff680727a84a38b
bzeitschel <bzeitschel@eos-uptrade.de>
2018-10-05 23:00:52 +0200

    safegame angefangen

---
+++ Assets/Scripts/SafeData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SafeData
+{
+    public static SafeData current;
+    public LevelData level1;
+    public LevelData level2;
+
+
+    public SafeData()
+        {
+            level1 = new LevelData();
+            level2 = new LevelData();
+           
+        }
+}
