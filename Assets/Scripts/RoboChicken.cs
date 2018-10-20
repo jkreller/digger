3f64ff632672ae88b0954562431e54be4af4d9b8
bzeitschel <bzeitschel@eos-uptrade.de>
2018-10-20 13:55:55 +0200

    chicks

---
+++ Assets/Scripts/RoboChicken.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboChicken : RoboEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Fixed Update is called once per frame
+    void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
