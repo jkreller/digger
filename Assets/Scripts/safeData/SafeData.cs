b5261657717cb80bc3f89e715fc473a09d0aec8a
bzeitschel <bzeitschel@eos-uptrade.de>
2018-11-05 09:49:24 +0100

    speicher bug behoben

--- Assets/Scripts/safeData/SafeData.cs
+++ Assets/Scripts/safeData/SafeData.cs
@@ -7,6 +7,11 @@ public class SafeData
     public static SafeData current;
     public List<LevelData> levels;
     public int diamonds;
+    public int redDiamonds;
+    public int greenDiamonds;
+    public int yellowDiamonds;
+    public int blackDiamonds;
+    public int specialDiamonds;
     public SafeData(){
         
     }
